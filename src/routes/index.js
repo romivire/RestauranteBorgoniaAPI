5a3a3b6057990200931a4686733b73bdd4921838
LAPTOP-V68SKF4G\romiv <romivire@gmail.com>
2021-06-01 21:10:34 -0300

    Routes modification

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,17 @@ const router = Router();
 let multer = require('multer');
 let upload = multer();
 
-const { getReservaByID, getPlatos, updateReserva } = require('../controllers/index.controller');
+const { getReservaByID, getPlatos, updateReserva, getImagePlatos, createReserva, getPlatosByCategoria, getPlatosVegetarianos, getPlatosNoVegetarianos } = require('../controllers/index.controller');
 
 router.get('/reserva/:id', getReservaByID);
 router.get('/platos', getPlatos);
+router.get('/platos/:categoria', getPlatosByCategoria);
+router.get('/platosVegetarianos', getPlatosVegetarianos);
+router.get('/platosNoVegetarianos', getPlatosNoVegetarianos);
+router.get('/platos/images/:id', getImagePlatos);
+router.post('/reserva/create',upload.none(), createReserva);
 router.post('/reserva/:id', upload.none(), updateReserva);
+
 
 
 module.exports = router;
