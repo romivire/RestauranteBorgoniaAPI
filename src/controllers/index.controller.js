54030ab330ba4e616da1ab3d2e95050f3f980984
LAPTOP-V68SKF4G\romiv <romivire@gmail.com>
2021-06-13 14:23:12 -0300

    fix issue #2

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -11,12 +11,17 @@ const pool = new Pool({
 
 
 const getReservaByID = async (req, res) => {
-    const id = parseInt(req.params.id);
-    const response = await pool.query('SELECT * FROM reservas WHERE id = $1', [id]);
-    if(response.rows.length >0)
-        res.status(200).json(response.rows);
-    else
-        res.sendStatus(404);
+    try{
+        const id = parseInt(req.params.id);
+        const response = await pool.query('SELECT * FROM reservas WHERE id = $1', [id]);
+        if(response.rows.length >0)
+            res.status(200).json(response.rows);
+        else
+            res.sendStatus(404);
+    }
+    catch(e){
+        res.status(404).json('Pedido incorrecto');
+    }
 };
 
 const getPlatos = async (req, res) => {
@@ -25,9 +30,14 @@ const getPlatos = async (req, res) => {
 };
 
 const getPlatosByCategoria = async (req, res) => {
-    const categoria = req.params.categoria;
-    const response = await pool.query('SELECT id,nombre,precio,vegetariano,categoria_plato FROM platos WHERE categoria_plato = $1',[categoria]);
-    res.status(200).json(response.rows);
+    const categoria = parseInt(req.params.categoria);
+    if(categoria){
+        res.status(404).json('Pedido incorrecto');
+    }
+    else{
+        const response = await pool.query('SELECT id,nombre,precio,vegetariano,categoria_plato FROM platos WHERE categoria_plato = $1',[req.params.categoria]);
+        res.status(200).json(response.rows);
+    }
 };
 
 const getPlatosVegetarianos = async (req, res) => {
@@ -41,42 +51,50 @@ const getPlatosNoVegetarianos = async (req, res) => {
 };
 
 const getImagePlatos = async (req, res) => {
-    const id = parseInt(req.params.id);
-    const fs = require('fs');
-    const response = await pool.query("SELECT encode(imagen,'base64') FROM platos WHERE id = $1", [id]);
-    if(response.rows.length >0){
-        var respuesta=Buffer.from(response.rows[0].encode,'base64');
-        var rta=respuesta.toString('utf-8');
-        const mimeType = 'image/png';
-    
-        res.status(200).send(`<img src="data:${mimeType};base64,${rta}" />`);
+    try{
+        const id = parseInt(req.params.id);
+        const fs = require('fs');
+        const response = await pool.query("SELECT encode(imagen,'base64') FROM platos WHERE id = $1", [id]);
+        if(response.rows.length >0){
+            var respuesta=Buffer.from(response.rows[0].encode,'base64');
+            var rta=respuesta.toString('utf-8');
+            const mimeType = 'image/png';
+        
+            res.status(200).send(`<img src="data:${mimeType};base64,${rta}" />`);
+        }
+        else{
+            res.sendStatus(404);
+        }
     }
-    else{
-        res.sendStatus(404);
+    catch(e){
+        res.status(404).json("Pedido incorrecto");
     }
 };
 
 const updateReserva = async (req, res) => {
     const id = parseInt(req.params.id);
     const estado='Pendiente'
-    try{
-        var data = req.body;
+    if (id){
+            var data = req.body;
 
-        const response =await pool.query('UPDATE reservas SET fecha = $1, hora = $2, cantidad_personas = $3,observacion = $4,estado = $5 WHERE id = $6', [
-            data.fecha,
-            data.hora,
-            data.cantidad,
-            data.observacion,
-            estado,
-            id
-        ]);
-        res.status(200).json('Reserva modificada');
+            const response =await pool.query('UPDATE reservas SET fecha = $1, hora = $2, cantidad_personas = $3,observacion = $4,estado = $5 WHERE id = $6', [
+                data.fecha,
+                data.hora,
+                data.cantidad,
+                data.observacion,
+                estado,
+                id
+            ]);
+            if(response.rowCount>0){
+                res.status(200).json('Reserva modificada');
+            }
+            else{
+                res.status(404).json('No se pudo modificar la reserva porque los valores son invalidos o la reserva no existe');
+            }
     }
-    catch(e){
-        res.status(404).json('No se pudo modificar la reserva porque los valores son invalidos o la reserva no existe');
+    else{
+        res.status(404).json("Pedido incorrecto");  
     }
-
-    
 };
 
 const createReserva = async (req, res) => {   
