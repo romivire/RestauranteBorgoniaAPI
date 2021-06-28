e76c70cc246beb9510ff60a98011a5ea46a3f400
LAPTOP-V68SKF4G\romiv <romivire@gmail.com>
2021-06-28 15:07:29 -0300

    install cors and allow reading API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 
 const app = express();
@@ -9,6 +10,17 @@ app.use(express.urlencoded({extended: true}));
 
 // Routes
 app.use(require('./routes/index'));
+app.use(cors);
+
+app.use(function(req,res,next){
+  res.setHeader('Access-Control-Allow-Origin','*');
+
+  res.setHeader('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type');
+
+  next();
+});
 
 app.get("/", function (req, res) {
     res.send("<h1>API-romivire-servicioWeb</h1>")
