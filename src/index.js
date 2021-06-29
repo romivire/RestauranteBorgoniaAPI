43c05339ec2e2c27644406e2a471de481029baee
LAPTOP-V68SKF4G\romiv <romivire@gmail.com>
2021-06-29 12:39:35 -0300

    add header content-type

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(function(req,res,next){
 
   res.setHeader('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type,multipart/form-data');
 
   next();
 });
